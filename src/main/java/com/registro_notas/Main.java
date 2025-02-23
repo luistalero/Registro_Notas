d2cf4650b1f3bea83d2b4493a962bc773432b586
luistalero <luisalbertotaleromartinez@gmail.com>
2025-02-23 10:40:18 -0500

    feat: :sparkles: Se agrega toda la funcionalidad al programa...

--- src/main/java/com/registro_notas/Main.java
+++ src/main/java/com/registro_notas/Main.java
@@ -1,7 +1,89 @@
 package com.registro_notas;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nIngrese la cantidad de estudiantes: ");
+        int estudiantes = sc.nextInt();
+        sc.nextLine();
+
+        Map<String, Double> registroNotas = new HashMap<>();
+        for (int i = 0; i < estudiantes; i++) {
+            System.out.print("Ingrese el nombre del estudiante " + (i + 1) + ": ");
+            String nombre = sc.nextLine();
+
+            System.out.print("Ingrese la nota de " + nombre + ": ");
+            double nota = sc.nextDouble();
+            sc.nextLine();
+
+            registroNotas.put(nombre, nota);
+        }
+
+        int opcion;
+        do {
+            String menu = """
+            ================================
+            ===    ¿Que desea hacer?     ===
+            ================================
+            1. Lista completa de estudiantes
+            2. Buscar nota de estudiante
+            3. Estudiante con mejor promedio
+            4. Salir
+            """;
+            System.out.println(menu);
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Lista de estudiantes y sus notas:");
+                    for (Map.Entry<String, Double> entry : registroNotas.entrySet()) {
+                        System.out.println("Estudiante: " + entry.getKey() + ", Nota: " + entry.getValue());
+                    }
+                    break;
+
+                case 2:
+                    boolean buscarOtro;
+                    do {
+                        System.out.println("Ingrese El nombre de un estudiante para buscar su nota: ");
+                        String nombreBusqueda = sc.nextLine();
+                        if (registroNotas.containsKey(nombreBusqueda)) {
+                            System.out.println("La nota de " + nombreBusqueda + "es: " + registroNotas.get(nombreBusqueda));
+                        } else {
+                            System.out.println("El estudiante \"" + nombreBusqueda + "\" no esta en el registro.");
+                        }
+                        System.out.print("¿Desea buscar la nota de otro estudiante? (s/n): ");
+                        String respuesta = sc.nextLine();
+                        buscarOtro = respuesta.equalsIgnoreCase("s");
+                    } while (buscarOtro);
+                    break;
+
+                case 3:
+                    double notaMax = Collections.max(registroNotas.values());
+                    String mejorRendimiento = "";
+
+                    for (Map.Entry<String, Double> entry : registroNotas.entrySet()) {
+                        if (entry.getValue() == notaMax) {
+                            mejorRendimiento = entry.getKey();
+                        }
+                    }
+                    System.out.println("El estudiante con mejor rendimiento es: " + mejorRendimiento);
+                    System.out.println("Su nota es: " + notaMax);
+                    break;
+
+                case 4:
+                    System.out.println("Gracias por usar este programa...");
+                    break;
+
+                default:
+                    System.out.println("ESta opción no es valida....");
+                    break;
+            }
+        } while (opcion != 4);
+
+        sc.close();
     }
 }
